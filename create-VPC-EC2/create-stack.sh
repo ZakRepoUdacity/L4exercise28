d6e5c279e2badf68380174273e837d8712399ab0
Zakarya_Udacity <zak3ar@gmail.com>
2022-10-30 13:40:35 +0200

    c1

---
+++ create-VPC-EC2/create-stack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+### Script by Zakarya
+### zak3ar@gmail.com
+#
+aws cloudformation create-stack \
+--region=us-east-1 \
+--stack-name $1 \
+--template-body file://$2 \
+--parameters file://$3 \
+--profile $4
